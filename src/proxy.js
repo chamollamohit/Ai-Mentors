7a198e7714f890ad42674d96250bf697dba88ad2
chamollamohit <mohitchamolla@gmail.com>
2025-11-14 18:11:01 +0530

    removed middleware and added proxy

--- src/proxy.js
+++ src/proxy.js
@@ -1,6 +1,6 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-const isProtectedRoute = createRouteMatcher(['/api/conversations(.*)', "/select"]);
+const isProtectedRoute = createRouteMatcher(['/api/conversations(.*)']);
 
 export default clerkMiddleware(async (auth, req) => {
     if (isProtectedRoute(req)) await auth.protect();
